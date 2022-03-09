7b501245bf920652d3b0de01a602099e3ba46ed1
Alejandro Rizzuto <alejandro@Alejandros-MacBook-Air.local>
2022-03-09 20:02:20 -0300

    Adding more logic to PriceConverter

--- src/main/java/com/inditex/challenge/converter/PriceConverter.java
+++ src/main/java/com/inditex/challenge/converter/PriceConverter.java
@@ -1,6 +1,8 @@
 package com.inditex.challenge.converter;
 
+import com.inditex.challenge.dto.DatesToApply;
 import com.inditex.challenge.dto.PriceDTO;
+import com.inditex.challenge.dto.PriceResultDTO;
 import com.inditex.challenge.repository.model.Price;
 import org.modelmapper.ModelMapper;
 
@@ -10,17 +12,37 @@ import java.util.stream.Collectors;
 public class PriceConverter {
     private static final ModelMapper modelMapper = new ModelMapper();
 
-    public static List<Price> convert(List<PriceDTO> listOfPrices) {
+    public static List<Price> convertToEntity(List<PriceDTO> listOfPrices) {
         return listOfPrices.stream()
-                .map(PriceConverter::convertToEntity)
+                .map(PriceConverter::toEntity)
                 .collect(Collectors.toList());
     }
 
-    private static Price convertToEntity(PriceDTO priceDTO) {
+    public static List<PriceDTO> convertToDTO(List<Price> listOfPrices) {
+        return listOfPrices.stream()
+                .map(PriceConverter::toDto)
+                .collect(Collectors.toList());
+    }
+
+    private static Price toEntity(PriceDTO priceDTO) {
         return modelMapper.map(priceDTO, Price.class);
     }
 
-    private static PriceDTO convertToDto(Price price) {
+    private static PriceDTO toDto(Price price) {
         return modelMapper.map(price, PriceDTO.class);
+    }
+
+    public static PriceResultDTO convertToResponse(PriceDTO price) {
+        return PriceResultDTO.builder()
+                .productId(price.getProductId())
+                .chainId(price.getBrandId())
+                .dateToApply(getDatesToApply(price))
+                .aliquotToApply(price.getPriceList())
+                .finalPrice(price.getPrice())
+                .build();
+    }
+
+    private static DatesToApply getDatesToApply(PriceDTO price) {
+        return new DatesToApply(price.getStartDate(), price.getEndDate());
     }
 }
