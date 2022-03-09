0a7f98e05eb66329e4c67a387011ba4232d4f226
Alejandro Rizzuto <alejandro@Alejandros-MacBook-Air.local>
2022-03-09 14:39:23 -0300

    Price entity and DTO with its converter to switch between them

--- src/main/java/com/inditex/challenge/dto/PriceDTO.java
+++ src/main/java/com/inditex/challenge/dto/PriceDTO.java
@@ -1,2 +1,39 @@
-package com.inditex.challenge.dto;public class PriceDTO {
+package com.inditex.challenge.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class PriceDTO {
+
+    @JsonProperty("id")
+    private Long id = null;
+
+    @JsonProperty("brand_id")
+    private Integer brandId;
+
+    @JsonProperty("start_date")
+    private String startDate;
+
+    @JsonProperty("end_date")
+    private String endDate;
+
+    @JsonProperty("price_list")
+    private Integer priceList;
+
+    @JsonProperty("product_id")
+    private Integer productId;
+
+    @JsonProperty("priority")
+    private Integer priority;
+
+    @JsonProperty("price")
+    private BigDecimal price;
+
+    @JsonProperty("currency")
+    private String currency;
+
 }
