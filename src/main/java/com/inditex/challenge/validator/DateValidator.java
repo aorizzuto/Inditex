6a766c2a6772c6549a0142f214e55e278801be6d
Alejandro Rizzuto <alejandro@Alejandros-MacBook-Air.local>
2022-03-10 11:29:26 -0300

    Adding more info when validation throws exception

--- src/main/java/com/inditex/challenge/validator/DateValidator.java
+++ src/main/java/com/inditex/challenge/validator/DateValidator.java
@@ -2,20 +2,21 @@ package com.inditex.challenge.validator;
 
 import com.inditex.challenge.exception.ErrorCode;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.Locale;
+import java.text.SimpleDateFormat;
 
 public class DateValidator {
 
     public static void validateFormat(String applyDate) throws Exception {
 
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("uuuu-MM-dd", Locale.US);
+        SimpleDateFormat dateFormatterWithHHMMSS = new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss");
 
+        checkFormat(applyDate, dateFormatterWithHHMMSS);
+    }
+
+    private static void checkFormat(String applyDate, SimpleDateFormat dateFormatter) throws Exception {
         try {
-            LocalDate.parse(applyDate, dateFormatter);
-        } catch (DateTimeParseException e) {
+            dateFormatter.parse(applyDate);
+        } catch (Exception e) {
             throw new Exception(ErrorCode.BAD_DATE_FORMAT);
         }
     }
