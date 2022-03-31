ea708fcabba9cd494b6763dfa4cecbb4ee17d861
Alejandro Rizzuto <alejandro@Alejandros-MacBook-Air.local>
2022-03-31 15:23:56 -0300

    Using new Custom Exceptions in validators

--- src/main/java/com/inditex/challenge/validator/DateValidator.java
+++ src/main/java/com/inditex/challenge/validator/DateValidator.java
@@ -1,23 +1,31 @@
 package com.inditex.challenge.validator;
 
 import com.inditex.challenge.exception.ErrorCode;
+import com.inditex.challenge.exception.business.BadDateFormatException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.text.SimpleDateFormat;
 
 public class DateValidator {
 
-    public static void validateFormat(String applyDate) throws Exception {
+    private static final Logger logger = LoggerFactory.getLogger(DateValidator.class);
+
+    public static void validateFormat(String applyDate) throws BadDateFormatException {
+
+        logger.info("Chain validation begin.");
 
         SimpleDateFormat dateFormatterWithHHMMSS = new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss");
 
         checkFormat(applyDate, dateFormatterWithHHMMSS);
     }
 
-    private static void checkFormat(String applyDate, SimpleDateFormat dateFormatter) throws Exception {
+    private static void checkFormat(String applyDate, SimpleDateFormat dateFormatter) throws BadDateFormatException {
         try {
             dateFormatter.parse(applyDate);
         } catch (Exception e) {
-            throw new Exception(ErrorCode.BAD_DATE_FORMAT);
+            logger.error("Bad format in date: " + applyDate);
+            throw new BadDateFormatException(ErrorCode.BAD_DATE_FORMAT);
         }
     }
 }
