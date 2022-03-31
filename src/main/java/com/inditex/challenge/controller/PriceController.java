20800f24d40c7796164d776ed1326ffd2235abbe
Alejandro Rizzuto <alejandro@Alejandros-MacBook-Air.local>
2022-03-31 15:11:04 -0300

    Replace Service call for Interface call in Controller

--- src/main/java/com/inditex/challenge/controller/PriceController.java
+++ src/main/java/com/inditex/challenge/controller/PriceController.java
@@ -1,8 +1,8 @@
 package com.inditex.challenge.controller;
 
 import com.inditex.challenge.dto.PriceResultDTO;
-import com.inditex.challenge.service.impl.PriceServiceImpl;
-import org.springframework.beans.factory.annotation.Value;
+import com.inditex.challenge.service.IPriceService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PriceController {
 
-    PriceServiceImpl priceService;
+    /**
+     * Use @Qualifier annotation only in case we have multiple implementations of IPriceService interface
+     * I let @Qualifier annotation in here just to let you know I'm aware of the existence of it and its use
+     */
+    @Qualifier("priceServiceImpl")
+    IPriceService priceService;
 
-    public PriceController(PriceServiceImpl priceService) {
+    public PriceController(IPriceService priceService) {
         this.priceService = priceService;
     }
 
